13576c438a4d5f565d3cf0770b4c089af330134d
JSnim <js@slowalk.co.kr>
2019-03-01 23:21:39 +0900

    deploy.php 수정

--- deploy.php
+++ deploy.php
@@ -11,9 +11,9 @@ $webhook_ip = array();
 
 function ip_check($webhook_ip, $visit_ip) {
     $long_ip = ip2long($visit_ip);
-    if($long_ip != -1) {
+    if($long_ip !== -1) {
         foreach($webhook_ip as $pri_addr) {
-            list($start, $end) = explode('|', $pri_addr);
+            [$start, $end] = explode('|', $pri_addr);
             if($long_ip >= ip2long($start) && $long_ip <= ip2long($end)) {
                 return true;
             }
@@ -22,26 +22,28 @@ function ip_check($webhook_ip, $visit_ip) {
     return false;
 }
 
-function get_client_ip() {
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
+function get_client_ip()
+{
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
         $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+    } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
         $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+    } elseif (isset($_SERVER['HTTP_X_FORWARDED'])) {
         $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+    } elseif (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
         $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
+    } elseif(isset($_SERVER['HTTP_FORWARDED'])) {
         $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
+    } elseif(isset($_SERVER['REMOTE_ADDR'])) {
         $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
+    } else {
         $ipaddress = 'UNKNOWN';
+    }
     return $ipaddress;
 }
 
-if(ip_check($bitbucket_ip, get_client_ip()) == true || $_SERVER['HTTP_USER_AGENT'] == 'Bitbucket-Webhooks/2.0') {
-    echo shell_exec( "cd /home/ubuntu/public_html/webzine && /usr/bin/git pull 2>&1" );
+if($_SERVER['HTTP_USER_AGENT'] === 'Bitbucket-Webhooks/2.0' || ip_check($bitbucket_ip, get_client_ip()) === true) {
+    echo shell_exec( 'cd /home/ubuntu/public_html/webzine && /usr/bin/git pull 2>&1' );
 } else {
     echo 'Reject Access!';
     echo get_client_ip();
