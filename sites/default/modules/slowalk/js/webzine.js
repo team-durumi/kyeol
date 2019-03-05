a1d7cdf1e46a42a528aa9895b8433262467f717a
JSnim <js@slowalk.co.kr>
2019-03-05 15:54:41 +0900

    메인페이지 설정 모듈 완성 (단, 슬라이드는 작업중)

--- sites/default/modules/slowalk/js/webzine.js
+++ sites/default/modules/slowalk/js/webzine.js
@@ -1,32 +1,149 @@
 (function ($) {
     $(document).ready(function () {
-        $('.form-item-features input[type=checkbox]').click(function () {
-           let totalChk =  $('.form-item-features input[type=checkbox]:checked').length;
-           let thisTxt = $(this).parent().text();
-           if(totalChk === 0) {
-               $('#edit-grid #first span').remove();
-           }
+        //Vol select change 이벤트
+        $('#edit-wz-main-vol').change(function () {
+            $('.form-item-wz-main-features input[type=checkbox]').attr('checked', false);
+            $('#edit-grid #first').empty();
+            $('input[name=wz_main_first_feature]').val('');
+            $('#edit-grid #second').empty();
+            $('input[name=wz_main_second_feature]').val('');
+            $('#lists').empty();
+        });
 
-           if(totalChk === 1) {
-               let firstLabel = $('<span />').text(thisTxt);
+        //첫 로딩시 저정된 값이 있을 때
+        if($('input[name=wz_main_first_feature]').val()) {
+            $.post(Drupal.settings.Webzine.ajaxUrl, {nid:$('input[name=wz_main_first_feature]').val(), type:'node'}, function(res) {
+                let nodeUrl = $('<a />').attr('href', res.url).attr('data-nid', res.nid);
+                let title = $('<h2 />').text(res.title);
+                let img = $('<img />').attr('src', res.img);
+                let desc = $('<span />').text(res.body);
+                let box = $('<div />').addClass('text');
+                box.append(title).append(desc);
+                nodeUrl.append(img).append(box);
+                $('#edit-grid #first').append($('<h1 />').text(res.category));
+                $('#edit-grid #first').append(nodeUrl);
+            });
+        }
+
+        if($('input[name=wz_main_second_feature]').val()) {
+            $.post(Drupal.settings.Webzine.ajaxUrl, {nid:$('input[name=wz_main_second_feature]').val(), type:'node'}, function(res) {
+                let nodeUrl = $('<a />').attr('href', res.url).attr('data-nid', res.nid);
+                let title = $('<h2 />').text(res.title);
+                let img = $('<img />').attr('src', res.img);
+                let desc = $('<span />').text(res.body);
+                let box = $('<div />').addClass('text');
+                box.append(title).append(desc);
+                nodeUrl.append(img).append(box);
+                $('#edit-grid #second').append($('<h1 />').text(res.category));
+                $('#edit-grid #second').append(nodeUrl);
+                generateList();
+            });
+        }
+
+        //기사분류 클릭 이벤트
+        $('.form-item-wz-main-features input[type=checkbox]').click(function () {
+            let $this = $(this);
+            let val = $this.val();
+            if($('#edit-wz-main-vol').val() === '_none_') {
+                alert('먼저 Vol. 을 선택해 주세요.');
+                $('#edit-wz-main-vol').focus();
+                return false;
+            }
+
+            let totalChk =  $('.form-item-wz-main-features input[type=checkbox]:checked').length;
+            let thisTxt = $(this).parent().text();
+            if(totalChk === 0) {
+               $('#edit-grid #first').empty();
+               $('input[name=wz_main_first_feature]').val('');
+            }
+
+            if(totalChk === 1) {
+               let firstLabel = $('<h1 />').text(thisTxt);
                if($(this).attr('checked')) {
-                   $('#edit-grid #first').append(firstLabel);
+                   generateFirstFeature($this, firstLabel, val);
                } else {
-                   $('#edit-grid #second span').remove();
+                   $('#edit-grid #second').empty();
+                   $('input[name=wz_main_second_feature]').val('');
+                   $('#lists').empty();
                }
-           }
+            }
 
-           if(totalChk === 2) {
-               let secondLabel = $('<span />').text(thisTxt);
+            if(totalChk === 2) {
+               let secondLabel = $('<h1 />').text(thisTxt);
                if($(this).attr('checked')) {
-                   $('#edit-grid #second').append(secondLabel);
+                   generateSecondFeature($this, secondLabel, val);
                }
-           }
+            }
 
-           if(totalChk === 3) {
+            if(totalChk === 3) {
                alert('2개까지 선택이 가능합니다.');
                $(this).attr("checked", false);
-           }
+            }
         });
+
+        function generateFirstFeature($this, label, category)
+        {
+            $.post(Drupal.settings.Webzine.ajaxUrl, {vol: $('#edit-wz-main-vol').val(), category:category, type:'features'}, function(res) {
+                if(res.length === 0) {
+                    alert('해당하는 콘텐츠가 존재하지 않습니다.');
+                    $this.attr('checked', false);
+                } else {
+                    let nodeUrl = $('<a />').attr('href', res.url).attr('data-nid', res.nid);
+                    let title = $('<h2 />').text(res.title);
+                    let img = $('<img />').attr('src', res.img);
+                    let desc = $('<span />').text(res.body);
+                    let box = $('<div />').addClass('text');
+                    box.append(title).append(desc);
+                    nodeUrl.append(img).append(box);
+                    $('#edit-grid #first').append(label);
+                    $('#edit-grid #first').append(nodeUrl);
+                    $('input[name=wz_main_first_feature]').val(res.nid);
+                }
+            });
+        }
+
+        function generateSecondFeature($this, label, category)
+        {
+            $.post(Drupal.settings.Webzine.ajaxUrl, {vol: $('#edit-wz-main-vol').val(), category:category, type:'features'}, function(res) {
+                if(res.length === 0) {
+                    alert('해당하는 콘텐츠가 존재하지 않습니다.');
+                    $this.attr('checked', false);
+                } else {
+                    let nodeUrl = $('<a />').attr('href', res.url).attr('data-nid', res.nid);
+                    let title = $('<h2 />').text(res.title);
+                    let img = $('<img />').attr('src', res.img);
+                    let desc = $('<span />').text(res.body);
+                    let box = $('<div />').addClass('text');
+                    box.append(title).append(desc);
+                    nodeUrl.append(img).append(box);
+                    $('#edit-grid #second').append(label);
+                    $('#edit-grid #second').append(nodeUrl);
+                    $('input[name=wz_main_second_feature]').val(res.nid);
+                    generateList();
+                }
+            });
+        }
+
+        function generateList()
+        {
+            let nid = [];
+            $.each($('#edit-grid a'), function(i, v) {
+               nid.push($(v).data('nid'));
+            });
+            $.post(Drupal.settings.Webzine.ajaxUrl, {vol:$('#edit-wz-main-vol').val(), nid:nid, type:'articles'}, function(res) {
+               $.each(res, function(i, v) {
+                   let item = $('<div />').addClass('list-item');
+                   let nodeUrl = $('<a />').attr('href', v.url).attr('data-nid', v.nid);
+                   let title = $('<h2 />').text(v.title);
+                   let img = $('<img />').attr('src', v.img);
+                   let desc = $('<span />').text(v.body);
+                   let box = $('<div />').addClass('text');
+                   box.append(title).append(desc);
+                   nodeUrl.append(img).append(box);
+                   item.append(nodeUrl);
+                   $('#lists').append(item);
+               });
+            });
+        }
     });
 })(jQuery);
---
+++ sites/default/modules/slowalk/slowalk_ajax.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: js
+ * Date: 2019-03-05
+ * Time: 13:37
+ */
+
+function webzine_ajax_callback()
+{
+    if(isset($_POST['type'])) {
+        $type = $_POST['type'];
+        $return = array();
+        $catLabel = array(
+            2 => '연구자의 말',
+            3 => '논평',
+            4 => '에세이',
+            5 => '자료해제',
+            6 => '연구자와 지원단체',
+            7 => '인터뷰'
+        );
+        switch ($type) {
+            case 'features':
+                $vol = $_POST['vol'];
+                $category = $_POST['category'];
+                $query = new EntityFieldQuery();
+                $result = $query->entityCondition('entity_type', 'node')
+                    ->entityCondition('bundle', 'article')
+                    ->propertyCondition('status', 1)
+                    ->fieldCondition('field_vol', 'tid', $vol)
+                    ->fieldCondition('field_category', 'tid', $category)
+                    ->propertyOrderBy('created', 'DESC')
+                    ->range(0,1)
+                    ->execute();
+                if($result['node']) {
+                    $nid = key($result['node']);
+                    $node = node_load($nid);
+                    $return = array(
+                        'nid' => $nid,
+                        'url' => '/node/'.$nid,
+                        'title' => $node->title,
+                        'img' => file_create_url($node->field_image['und'][0]['uri']),
+                        'body' => text_summary($node->body['und'][0]['value'], 'plain_text', 100)
+                    );
+                }
+                break;
+            case 'articles':
+                $vol = $_POST['vol'];
+                $nid = $_POST['nid'];
+                $query = new EntityFieldQuery();
+                $result = $query->entityCondition('entity_type', 'node')
+                    ->entityCondition('bundle', 'article')
+                    ->propertyCondition('status', 1)
+                    ->propertyCondition('nid', $nid, 'NOT IN')
+                    ->fieldCondition('field_vol', 'tid', $vol)
+                    ->propertyOrderBy('created', 'DESC')
+                    ->execute();
+                if($result['node']) {
+                    $nids = array_keys($result['node']);
+                    $nodes = node_load_multiple($nids);
+                    foreach($nodes as $node) {
+                        $tid = $node->field_category['und'][0]['tid'];
+                        $nid = $node->nid;
+                        $return[$nid] = array(
+                            'nid' => $nid,
+                            'url' => '/node/'.$nid,
+                            'title' => $node->title,
+                            'category' => $catLabel[$tid],
+                            'img' => file_create_url($node->field_image['und'][0]['uri']),
+                            'body' => text_summary($node->body['und'][0]['value'], 'plain_text', 100)
+                        );
+                    }
+                }
+                break;
+            case 'node':
+                if(isset($_POST['nid'])) {
+                    $nid = $_POST['nid'];
+                    $node = node_load($nid);
+                    $tid = $node->field_category['und'][0]['tid'];
+                    $return = array(
+                        'nid' => $nid,
+                        'url' => '/node/'.$nid,
+                        'title' => $node->title,
+                        'category' => $catLabel[$tid],
+                        'img' => file_create_url($node->field_image['und'][0]['uri']),
+                        'body' => text_summary($node->body['und'][0]['value'], 'plain_text', 100)
+                    );
+                }
+                break;
+        }
+        drupal_json_output($return);
+    }
+
+}
