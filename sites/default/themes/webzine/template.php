8ca0ecc51f44aceded995eab7253a18fb792cae8
JSnim <js@slowalk.co.kr>
2019-03-07 17:24:52 +0900

    지난호 보기, 모아보기 템플릿 생성

--- sites/default/themes/webzine/template.php
+++ sites/default/themes/webzine/template.php
@@ -15,3 +15,12 @@ function webzine_js_alter(&$javascript)
     $javascript['misc/jquery.js']['version'] = '3.3.1';
     $javascript['misc/jquery.js']['data'] = 'https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js';
 }
+
+function webzine_preprocess_page(&$variables)
+{
+    //호수 템플릿 설정
+    if(isset($variables['page']['content']['system_main']['term_heading']['term']['#bundle']) &&
+        $variables['page']['content']['system_main']['term_heading']['term']['#bundle'] === 'vol') {
+        $variables['theme_hook_suggestions'][] = 'page__vol';
+    }
+}
