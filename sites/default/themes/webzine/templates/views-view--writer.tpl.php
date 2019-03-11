b2bfdc2220a881acb4e13f82c537838fadc52226
JSnim <js@slowalk.co.kr>
2019-03-11 19:33:02 +0900

    모아보기 필자 완료

---
+++ sites/default/themes/webzine/templates/views-view--writer.tpl.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * Main view template.
+ *
+ * Variables available:
+ * - $classes_array: An array of classes determined in
+ *   template_preprocess_views_view(). Default classes are:
+ *     .view
+ *     .view-[css_name]
+ *     .view-id-[view_name]
+ *     .view-display-id-[display_name]
+ *     .view-dom-id-[dom_id]
+ * - $classes: A string version of $classes_array for use in the class attribute
+ * - $css_name: A css-safe version of the view name.
+ * - $css_class: The user-specified classes names, if any
+ * - $header: The view header
+ * - $footer: The view footer
+ * - $rows: The results of the view query, if any
+ * - $empty: The empty text to display if the view is empty
+ * - $pager: The pager next/prev links to display, if any
+ * - $exposed: Exposed widget form/info to display
+ * - $feed_icon: Feed icon to display, if any
+ * - $more: A link to view more, if any.
+ *
+ * @ingroup views_templates
+ */
+?>
+<div class="fc_box01">
+    <p class="td01">총 <em>164명</em>의 필자가 참여했습니다.</p>
+</div>
+
+<div class="<?php print $classes; ?>">
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+        <?php print $title; ?>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($rows): ?>
+        <div class="lc03">
+            <?php print $rows; ?>
+        </div>
+    <?php elseif ($empty): ?>
+        <div class="view-empty">
+            <?php print $empty; ?>
+        </div>
+    <?php endif; ?>
+
+</div>
+
+<?php if ($pager): ?>
+    <?php print $pager; ?>
+<?php endif; ?>
