ef012234b4c1ad766d5d256de3f7624f0a29d9cb
mychricia <mj@slowalk.co.kr>
2019-03-15 21:28:04 +0900

    - 맨위로 버튼 추가

--- sites/default/themes/webzine/js/webzine.js
+++ sites/default/themes/webzine/js/webzine.js
@@ -239,26 +239,6 @@ $(document).ready(function() {
 	    });
 	}
 
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function() {scrollFunction()};
-
-	function scrollFunction() {
-		if($("#toTop").length){
-			if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-				document.getElementById("toTop").style.display = "block";
-			} else {
-				document.getElementById("toTop").style.display = "none";
-			}
-		}
-	}
-
-
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-	    document.body.scrollTop = 0; // For Safari
-	    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-	}
-
 });
 
 //PC버젼 초기화
@@ -306,6 +286,24 @@ function form_validation(){
          }
     });
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+	if($("#toTop").length){
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("toTop").style.display = "block";
+		} else {
+			document.getElementById("toTop").style.display = "none";
+		}
+	}
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 }
