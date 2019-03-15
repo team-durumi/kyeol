50fdec545f67b2fc8fa1ebcca85c3b3f534b0e0b
mychricia <mj@slowalk.co.kr>
2019-03-15 18:58:08 +0900

    - 모달창 반응형으로 변경 - 테마 설명글 오타수정 - jquery ui 커스텀 스크립트 추가

--- sites/default/themes/webzine/js/webzine.js
+++ sites/default/themes/webzine/js/webzine.js
@@ -196,12 +196,47 @@ $(document).ready(function() {
             box.dialog({
             	modal: true,
                 title:title,
-                width:'80%',
-                maxWidth:590,
+			    width: 'auto', // overcomes width:'auto' and maxWidth bug
+			    maxWidth: 600,
+			    height: 'auto',
+			    modal: true,
+			    fluid: true, //new option
 				position: { my: "center", at: "center", of: window }
             });
         });
     });
+	// on window resize run function
+	$(window).resize(function () {
+	    fluidDialog();
+	});
+
+	// catch dialog if opened within a viewport smaller than the dialog width
+	$(document).on("dialogopen", ".ui-dialog", function (event, ui) {
+	    fluidDialog();
+	});
+
+	function fluidDialog() {
+	    var $visible = $(".ui-dialog:visible");
+	    // each open dialog
+	    $visible.each(function () {
+	        var $this = $(this);
+	        var dialog = $this.find(".ui-dialog-content").data("ui-dialog");
+	        // if fluid option == true
+	        if (dialog.options.fluid) {
+	            var wWidth = $(window).width();
+	            // check window width against dialog width
+	            if (wWidth < (parseInt(dialog.options.maxWidth) + 50))  {
+	                // keep dialog from filling entire screen
+	                $this.css("max-width", "90%");
+	            } else {
+	                // fix maxWidth bug
+	                $this.css("max-width", dialog.options.maxWidth + "px");
+	            }
+	            //reposition dialog
+	            dialog.option("position", dialog.options.position);
+	        }
+	    });
+	}
 });
 
 //PC버젼 초기화
--- sites/default/themes/webzine/template.php
+++ sites/default/themes/webzine/template.php
@@ -66,6 +66,7 @@ function webzine_preprocess_page(&$variables)
     }
     drupal_add_js(array('Webzine' => array('vol' => $main->baseUrl.'/vol/'.$main->vol())), 'setting');
     drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js', array('type' => 'external', 'scope' => 'header', 'group' => JS_LIBRARY));
+    drupal_add_js(drupal_get_path('theme', 'webzine') . 'js/jquery.dialogOptions.js', array('type' => 'file', 'scope' => 'header', 'group' => JS_LIBRARY));
     drupal_add_js('https://cdn.jsdelivr.net/npm/jquery-validation@1.19.0/dist/jquery.validate.min.js', array('type' => 'external', 'scope' => 'header', 'group' => JS_LIBRARY ));
     drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js', array('type' => 'external', 'scope' => 'header', 'group' => JS_THEME ));
     drupal_add_js(drupal_get_path('module', 'ckeditor') . '/plugins/cavacnote/jquery/jquery.cavacnote.js', array('type' => 'file', 'scope' => 'header', 'group' => JS_LIBRARY));
