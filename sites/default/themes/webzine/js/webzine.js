5d793d6c6436a5512a046678b5a97f4cd40dbb7e
mychricia <mj@slowalk.co.kr>
2019-03-15 21:29:37 +0900

    - 맨위로 버튼 추가

--- sites/default/themes/webzine/js/webzine.js
+++ sites/default/themes/webzine/js/webzine.js
@@ -286,6 +286,11 @@ function form_validation(){
          }
     });
 
+
+
+}
+
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -304,6 +309,4 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 }
