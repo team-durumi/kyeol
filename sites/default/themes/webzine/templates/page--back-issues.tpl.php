9043c000fee53796ba6ff2290be1da1bbf55461e
mychricia <mj@slowalk.co.kr>
2019-03-18 19:43:50 +0900

    - 지난호 보기 .inner overflow hidden해제

--- sites/default/themes/webzine/templates/page--back-issues.tpl.php
+++ sites/default/themes/webzine/templates/page--back-issues.tpl.php
@@ -92,6 +92,7 @@
                     <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
                     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
                     <?php print render($page['content']); ?>
+                    <div class="clear"></div>
                     <?php print $feed_icons; ?>
                 </div>
             </section>
