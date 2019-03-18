454c7d30002d30ab0606caf224626cfda718e97a
mychricia <mj@slowalk.co.kr>
2019-03-18 19:45:52 +0900

    - 태그검색 문구변경

--- sites/default/themes/webzine/templates/views-view--search-term.tpl.php
+++ sites/default/themes/webzine/templates/views-view--search-term.tpl.php
@@ -30,7 +30,7 @@
 
 <?php if(isset($_GET['key']) && !empty($_GET['key']) && isset($view->total_rows)): ?>
     <div class="fc_box01 fc_box01_01 tags">
-        <p class="td01 leftF"><b>"<?php print $_GET['key'];?>"</b>태그와 관련된 컨텐츠는 <em><?php print $view->total_rows;?></em>건입니다.</p>
+        <p class="td01 leftF"><b>"<?php print $_GET['key'];?>"</b>태그와 관련된 기사는 <em><?php print $view->total_rows;?></em>건입니다.</p>
     </div>
 <?php endif;?>
 
